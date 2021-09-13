e650313f2f2ecb6e2e5fd8e8d19865ab71118aa9
Tsapyk Yelyzaveta <lizatsapik@gmail.com>
2021-09-13 23:15:19 +0300

    Nothing special but User

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ function App() {
 function Header() {
   return (
         <header className="App-header">
+            <User/>
+            <Select/>
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -28,5 +30,30 @@ function Header() {
   );
 }
 
+function User({name = "unknown"}) {
+    return (
+        <div className="App-user">
+            <p>I am <span id="user-status">{name}</span></p>
+        </div>
+    );
+}
+function Select() {
+    return (
+        <ul className="App-select">
+            <li>
+                <label htmlFor="student">Student</label>
+                <input type="radio" name="who" id="student"/>
+            </li>
+            <li>
+                <label htmlFor="teacher">Teacher</label>
+                <input type="radio" name="who" id="teacher"/>
+            </li>
+            <li>
+                <label htmlFor="person">Just a person</label>
+                <input type="radio" name="who" id="person"/>
+            </li>
+        </ul>
+    );
+}
 
 export default App;
